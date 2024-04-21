55979489b782574c3d94659bb9663692cddd5e4f
MrCap4a <2212denis@gmail.com>
2024-04-21 20:45:47 +0300

    Completed task 1 and task 2

---
+++ Lab3T1/Lab3T1/Lab3T1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class money {
+public:
+    int count = 0;
+    int cost = 0;
+    money(int count, int cost) : count(count), cost(cost) {};
+
+    string get_info() {
+        return "Номинал: " + to_string(cost) + ", количество: " + to_string(count);
+    }
+
+    int get_sum() {
+        return count * cost;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    cout << "Номинал: ";
+    cin >> a;
+    cout << "Количество: ";
+    cin >> b;
+    money ten(a, b);
+    cout << ten.get_sum() << endl;
+    cout << ten.get_info() << endl;
+}
---
+++ Lab3T2/Lab3T2/Lab3T2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+class date {
+private:
+    int day = 1;
+    int month = 1;
+    int year = 2000;
+
+public:
+    date() {};
+    date(int day, int month, int year) : day(day), month(month), year(year) {};
+    ~date() {
+        cout << "Объект класса date уничтожен";
+    }
+
+    string get_info() {
+        return to_string(day) + "-" + to_string(month) + "-" + to_string(year) + ";";
+    }
+
+    bool is_leap() {
+        if (year % 4 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    void increase() {
+        int max_days;
+        if (is_leap()) {
+            map<int, int> monthes = {
+                {1, 31},
+                {2, 28},
+                {3, 31},
+                {4, 30},
+                {5, 31},
+                {6, 30},
+                {7, 31},
+                {8, 31},
+                {9, 30},
+                {10, 31},
+                {11, 30},
+                {12, 31}
+            };
+            max_days = monthes[month];
+        }
+        else {
+            map<int, int> monthes = {
+                {1, 31},
+                {2, 29},
+                {3, 31},
+                {4, 30},
+                {5, 31},
+                {6, 30},
+                {7, 31},
+                {8, 31},
+                {9, 30},
+                {10, 31},
+                {11, 30},
+                {12, 31}
+            };
+            max_days = monthes[month];
+        }
+        if (max_days < (day + 5)) {
+            day = 5 - (max_days - day);
+            month += 1;
+        }
+        else {
+            day += 5;
+        }
+        if (month == 13) {
+            month = 1;
+            year += 1;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    date this_day(21, 4, 2024);
+    cout << this_day.is_leap() << endl;
+    cout << this_day.get_info() << endl;
+    this_day.increase();
+    cout << this_day.get_info() << endl;
+}
